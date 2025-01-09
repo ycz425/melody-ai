d86ac3a999eabd3f8e73b2a7678d72a5e475df58
John Zhang <23zhanyuc@gmail.com>
2025-01-09 00:10:37 -0800

    Implement loading function

--- preprocess.py
+++ preprocess.py
@@ -1,1 +1,26 @@
+import os
+import music21
+from music21 import environment
+
+
+KERN_DATASET_PATH = 'data/europa/deutschl/test'
+ACCEPTABLE_DURATIONS = {0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 3.0, 4.0}
+
+
+def load(dataset_path: str) -> list[music21.stream.Score]:
+    songs = []
+    for dirpath, dirnames, filenames in os.walk(dataset_path):
+        for filename in filenames:
+            if filename.endswith('.krn'):
+                songs.append(music21.converter.parse(os.path.join(dirpath, filename), format='kern'))
+
+    return songs
+
+
+def preprocess(dataset_path: str):
+    print('Loading songs...')
+    songs = load(dataset_path)
+    print(f'Loaded {len(songs)} songs.')
+
+
 
